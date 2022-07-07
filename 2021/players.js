faa2f29ba3e2c38e36aa4f4e22b299e7054d8008
luke <lukevance90@gmail.com>
2022-07-06 21:35:57 -0400

    added draft lookup data to players

--- 2021/players.js
+++ 2021/players.js
@@ -7,12 +7,20 @@ const onlyUnique = (val, index, self) => self.indexOf(val) === index;
 const allNameStrings = allGamesRegularSeason.map(game => game.name);
 const uniquePlayers = allNameStrings.filter(onlyUnique);
 
-// const getRoundDrafted = (name, draftBoard) => {
-
-// }
+const checkDrafted = (player, draftBoard) => {
+    const matches = draftBoard.filter(pick => pick.player_name === player.name);
+    if (matches.length === 1) {
+        return matches[0];
+    } else if (matches.length > 1) {
+        return matches.filter(match => match.player_position === player.position)[0];
+    } else {
+        return null;
+    }
+}
 
 const uniquePlayersWithGames = uniquePlayers.map(playerName => {
     const playerGames = allGamesRegularSeason.filter(game => game.name === playerName);
+    const playerDrafted = checkDrafted(playerGames[0], draftBoard2021);
     return {
         name: playerName,
         id: playerGames[0].playerId,
@@ -21,7 +29,8 @@ const uniquePlayersWithGames = uniquePlayers.map(playerName => {
                     .map(teamId => teams.find(tm => tm.id === teamId).name),
         starts: playerGames.filter(game => game.starter).length,
         benched: playerGames.filter(game => !game.starter).length,
-        // TODO: add round_drafted
+        drafted_round: playerDrafted ? playerDrafted.round_no : 0,
+        sleeper_id: playerDrafted ? playerDrafted.player_id: "0000",
     };
 });
 
