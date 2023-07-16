c15df8be2b74ffb011ca064a6bce38988bde0cd8
Luke Vance <lukevance90@gmail.com>
2023-07-15 21:48:37 -0400

    added remaining 2022 data

--- 2022/weeklyRosters/index.js
+++ 2022/weeklyRosters/index.js
@@ -2,6 +2,15 @@ const week1 = require('./wk1.json');
 const week2 = require('./wk2.json');
 const week3 = require('./wk3.json');
 const week4 = require('./wk4.json');
+const week5 = require('./wk5.json');
+const week6 = require('./wk6.json');
+const week7 = require('./wk7.json');
+const week8 = require('./wk8.json');
+const week9 = require('./wk9.json');
+const week10 = require('./wk10.json');
+const week11 = require('./wk11.json');
+const week12 = require('./wk12.json');
+const week13 = require('./wk13.json');
 
 const regularSeason = [week1, week2, week3, week4];
 
